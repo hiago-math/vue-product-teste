e1f41196cb3ffd24f587e45fe269361616b98c22
Hiago Matheus <oliverhia.math@gmail.com>
2022-11-08 19:40:36 -0300

    logout

--- src/router/api.js
+++ src/router/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import Cookie from "js-cookie"
 
 const api = axios.create({
-    baseURL: "http://product.localhost/api",
+    baseURL: "https://api-product-teste.herokuapp.com/api",
     headers: {
         'Content-Type': 'multipart/form-data, application/json',
         'Acess': 'application/json',
