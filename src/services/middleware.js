be3237cab22a2f3ca3e7cf29c1c533bc141d7148
Hiago Matheus <oliverhia.math@gmail.com>
2022-11-08 23:02:37 -0300

    fix-command

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,8 +1,14 @@
 import Cookie from 'js-cookie'
+import api from "@/router/api";
 
 export default {
 
     auth(to, from, next) {
+        api.get('/auth/hasToken')
+            .catch(() => {
+                Cookie.remove('_auth_app_token')
+            })
+
         const token = Cookie.get('_auth_app_token');
 
         if (!token) {
